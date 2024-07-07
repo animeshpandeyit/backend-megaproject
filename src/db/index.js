98029b16b6c12fbd6817bec03ae339d7ccfece16
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2024-07-07 19:45:48 +0530

    util code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
+      `${process.env.MONGO_URL}/${DB_NAME}`
     );
 
     console.log(
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-// require("dotenv").config();\
+// require("dotenv").config();
 
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongodb connection error/failed !!!", error);
+  });
 
 /*
 import express from "express";
 const app = express();
 (async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
 
     app.on("error", (error) => {
       console.log("error: ", error);
